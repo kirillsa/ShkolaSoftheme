0f9ebc625c83a0a60c2ad1176a378623bbbe0daf
Kirill Savitskiy <kirillsavik@gmail.com>
2017-11-07 23:28:34 +0200

    4 lection Figure in console complete

--- CSharpHW/5 lection/Figures_in_console/Figures_in_console/Program.cs
+++ CSharpHW/5 lection/Figures_in_console/Figures_in_console/Program.cs
@@ -8,53 +8,135 @@ namespace Figures_in_console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Triangle(int dim)
         {
-            int num;
-            bool exitFlag = true;
-            while (exitFlag)
+            var str = "";
+            for (int i = 0; i < dim; i++)
             {
-                num = int.Parse(Console.ReadLine());
-                for (int i = 0; i < num; i++)
+                Console.WriteLine(str += "* ");
+            }
+        }
+
+        static void Square(int dim)
+        {
+            var str = "";
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
                 {
-                    for (int j = 0; j <= i; j++)
-                    {
-                        Console.Write("* ");
-                    }
-                    Console.Write("\n");
+                    Console.Write("* ");
                 }
                 Console.Write("\n");
-                for (int i = 0; i < num; i++)
+            }
+        }
+
+        static void Romb(int dim)
+        {
+            var str = "";
+            var midDim = dim / 2;
+            if (dim % 2 == 1)
+            {
+                for (int i = 0; i < dim; i++)
                 {
-                    for (int j = 0; j < num; j++)
+                    for (int j = 0; j < dim; j++)
                     {
-                        Console.Write("* ");
-                    }
-                    Console.Write("\n");
-                }
-                Console.Write("\n");
-                for (int i = 0; i < 2*num-1; i++)
-                {
-                    for (int j = 0; j < 2*num-1; j++)
-                    {
-                        if ((j-i) < num-1 || (j-i) >= num)
+                        if (i <= midDim)
                         {
-                            Console.Write("  ");
+                            if (j < (midDim - i) || (midDim + i) < j)
+                            {
+                                Console.Write("  ");
+                            }
+                            else
+                                Console.Write("* ");
                         }
                         else
-                        Console.Write("* ");
+                        {
+                            if (j < (midDim + i - dim + 1) || (midDim - i + dim - 1) < j)
+                            {
+                                Console.Write("  ");
+                            }
+                            else
+                                Console.Write("* ");
+                        }
                     }
                     Console.Write("\n");
                 }
-                /*for (int i = num-1; i > 0; i--)
+            }
+            else
+            {
+                for (int i = 0; i < dim; i++)
                 {
-                    for (int j = i; j > 0; j--)
+                    for (int j = 0; j < dim; j++)
                     {
-                        Console.Write("* ");
+                        if (i <= midDim)
+                        {
+                            if (j < (midDim - i - 1) || (midDim + i) < j)
+                            {
+                                Console.Write("  ");
+                            }
+                            else
+                                Console.Write("* ");
+                        }
+                        else
+                        {
+                            if (j < (midDim + i - dim) || (midDim - i + dim - 1) < j)
+                            {
+                                Console.Write("  ");
+                            }
+                            else
+                                Console.Write("* ");
+                        }
                     }
                     Console.Write("\n");
-                }*/
+                }
             }
+        }
+
+        static void Main(string[] args)
+        {
+            var dim = 0;
+            var figure = "";
+            var exitFlag = true;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Type \"q\" for exit");
+                Console.WriteLine("Choose the figure you want to see: t - for triangle, s - for square, r - for romb");
+                figure = Console.ReadLine();
+                if (figure == "q")
+                {
+                    exitFlag = false;
+                    continue;
+                }
+                if (figure != "t" && figure != "s" && figure != "r")
+                {
+                    continue;
+                }
+                Console.WriteLine("Enter the dimension of the figure:");
+                try
+                {
+                    dim = int.Parse(Console.ReadLine());
+                    switch (figure)
+                    {
+                        case "t":
+                            Triangle(dim);
+                            break;
+                        case "s":
+                            Square(dim);
+                            break;
+                        case "r":
+                            Romb(dim);
+                            break;
+                    }
+                    Console.Write("Press enter");
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            while (exitFlag);
         }
     }
 }
