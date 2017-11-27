83aa76183af7e6499f5fc8baa3235809e2c0194d
Kirill Savitskiy <kirillsavik@gmail.com>
2017-11-28 01:26:07 +0200

    14 lection completed

---
+++ CSharpHW/14 lection/14_Indexers/14_Indexers/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Indexers
+{
+    class Program
+    {
+        static int[] InputLotery(int[] arr)
+        {
+            var inputInt = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter 6 numbers from 1 to 9");
+                    Console.WriteLine($"Number {i + 1} is ");
+                    var inputS = Console.ReadLine();
+                    if (!int.TryParse(inputS, out inputInt))
+                    {
+                        continue;
+                    }
+                }
+                while (inputInt < 1 || inputInt > 9);
+                arr[i] = inputInt;
+            }
+            return arr;
+        }
+
+        static bool CheckTicket( RandomArr lotery, int[] ticket )
+        {
+            for (var i = 0; i < ticket.Length; i++)
+            {
+                for (var j = 0; j < lotery.GetLength(); j++)
+                {
+                    if (ticket[i] == lotery[j])
+                    {
+                        break;
+                    }
+                    else if (j == lotery.GetLength()-1 && ticket[i] != lotery[lotery.GetLength()-1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static void ShowArr(int[] arr)
+        {
+            Console.Write("It's your ticket: ");
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var arr = new int[6];
+            arr = InputLotery(arr);
+            var arrLotery = new RandomArr(arr.Length);
+            if (CheckTicket(arrLotery, arr))
+            {
+                Console.Clear();
+                Console.WriteLine("You win");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You lost");
+            }
+            ShowArr(arr);
+            arrLotery.ShowInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharpHW/14 lection/14_Indexers/14_Indexers/Random.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Indexers
+{
+    class RandomArr
+    {
+        private int[] _arr;
+
+        public RandomArr(int n)
+        {
+            _arr = new int[n];
+            var rand = new Random();
+            for (var i = 0; i < _arr.Length; i++)
+            {
+                _arr[i] = rand.Next(1,10);
+            }
+        }
+        public int this[int i] => _arr[i];
+
+        public int GetLength()
+        {
+            return _arr.Length;
+        }
+
+        public void ShowInfo()
+        {
+            Console.Write("It's a lotery numbers: ");
+            for (var i = 0; i < _arr.Length; i++)
+            {
+                Console.Write($"{_arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
