470d8746e8597836fb885bef322b43446a7f531e
Kirill Savitskiy <kirillsavik@gmail.com>
2017-12-06 18:00:22 +0200

    18 backed

---
+++ CSharpHW/18 lection/MobileOperator/MobileOperator/Operator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileOperator
+{
+    class Operator
+    {
+        public delegate void ServiceMessage(string str);
+        public event ServiceMessage Messaging;
+        public event ServiceMessage Calling;
+
+        private Dictionary<int, MobileAccount> _dictOfClients;
+
+        MobileAccount instSender;
+        MobileAccount instReciever;
+
+        public Operator()
+        {
+            _dictOfClients = new Dictionary<int, MobileAccount> ();
+        }
+
+        private MobileAccount InitClient(int number)
+        {
+            if (_dictOfClients.ContainsKey(number))
+            {
+                    return _dictOfClients[number];
+            }
+        return null;
+        }
+
+        private bool IsRealNumbers(int sender, int reciever)
+        {
+            instSender = InitClient(sender);
+            if (instSender is null)
+            {
+                Messaging.Invoke($"Wrong number of sender {sender}");
+                return false;
+            }
+            else
+            {
+                instReciever = InitClient(reciever);
+                if (instReciever is null)
+                { 
+                    Messaging.Invoke($"Wrong number of reciever {reciever}");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void AddClient(int number)
+        {
+            if (!_dictOfClients.ContainsKey(number))
+            {
+                _dictOfClients.Add(number, new MobileAccount(number));
+            }
+        }
+
+        public void Message(int sender, int reciever)
+        {
+            if (IsRealNumbers(sender, reciever))
+            {
+                if (Messaging != null)
+                { 
+                    Messaging.Invoke(instSender.MessageOut(instReciever));
+                    Messaging.Invoke(instReciever.MessageIn(instSender));
+                }
+            }
+        }
+
+        public void Call(int sender, int reciever)
+        {
+            if (IsRealNumbers(sender, reciever))
+            {
+                if (Calling != null)
+                {
+                    Calling.Invoke(instSender.CallOut(instReciever));
+                    Calling.Invoke(instReciever.CallIn(instSender));
+                }
+            }
+        }
+    }
+}
---
+++ CSharpHW/18 lection/MobileOperator/MobileOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mobOperator = new Operator();
+            for (var i = 0; i < 5; i++)
+            {
+                mobOperator.AddClient(i);
+            }
+
+            mobOperator.Messaging += Show_Message;
+            mobOperator.Calling += Show_Message;
+            mobOperator.Message(1, 3);
+            mobOperator.Message(1, 3);
+            mobOperator.Call(4,3);
+            mobOperator.Call(9,9);
+
+            Console.ReadKey();
+        }
+
+        private static void Show_Message(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
