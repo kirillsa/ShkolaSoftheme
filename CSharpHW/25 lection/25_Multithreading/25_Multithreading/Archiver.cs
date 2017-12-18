3f7f0181b5c278b0b68fdd2d13831f1ae67d4ed2
Kirill Savitskiy <kirillsavik@gmail.com>
2017-12-18 17:27:32 +0200

    25 lection only zipping

---
+++ CSharpHW/25 lection/25_Multithreading/25_Multithreading/Archiver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+using ZipFile = System.IO.Compression.ZipFile;
+
+namespace _25_Multithreading
+{
+    class Archiver
+    {
+        Semaphore sem = new Semaphore(Environment.ProcessorCount, Environment.ProcessorCount);
+
+        public Archiver(string path)
+        {
+            var thread = new Thread(Zipping);
+            thread.Start(path);
+        }
+
+        void Zipping(object obj)
+        {
+            sem.WaitOne();
+            //lock (obj)
+            {
+                string pathFile = obj.ToString();
+                var dirName = Path.GetDirectoryName(pathFile);
+                var fullZipName = dirName + "\\" + Path.GetFileNameWithoutExtension(pathFile) + ".zip";
+                var fileName = Path.GetFileName(pathFile);
+
+                //ZipFile.CreateFromDirectory( dirName.ToString(), zipName );
+                FastZip zip = new FastZip();
+                string filter = string.Format(@"{0};-\.zip$", fileName);
+                zip.CreateZip(fullZipName, dirName, false, filter);
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}");
+
+            }
+            sem.Release();
+        }
+    }
+}
---
+++ CSharpHW/25 lection/25_Multithreading/25_Multithreading/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDir = "asd";
+            Directory.EnumerateFiles(startDir);
+            GoThroughDirectories(startDir);
+            Console.ReadKey();
+        }
+
+        static void GoThroughDirectories(string startPath)
+        {
+            var dirsList = Directory.EnumerateDirectories(startPath);
+            foreach (var dir in dirsList)
+            {
+                GoThroughDirectories(dir);
+            }
+
+            var filesList = Directory.EnumerateFiles(startPath);
+            foreach (var file in filesList)
+            {
+                if (Path.GetExtension(file) == ".zip")
+                {
+                    continue;
+                }
+                else
+                {
+                    var archive = new Archiver(file);
+                }
+            }
+        }
+    }
+}
